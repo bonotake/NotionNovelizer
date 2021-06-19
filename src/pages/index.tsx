3ad524c4e8cfbb17f69fdf0f9b0c15f6c29403b1
Takeo Imai <takeo.bono@gmail.com>
2021-06-19 18:35:22 +0900

    extract Content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import type { PlainText } from '../libs/types'
 import useSWR from 'swr'
 
-const Main = () => {
+const Content: () => JSX.Element = () => {
     const { data, error } = useSWR<PlainText>('api/text');
     if (error) return <p>failed to load...</p>
     if (!data) return <p>loading...</p>
 
     const lines = data.text.split('\n');
-    return lines.map((line) => <React.Fragment>{line}<br /></React.Fragment>)
+    return <div>{lines.map((line) => {
+        return line.startsWith('[chapter') ?
+            <h3>{line.substring(9, line.length - 1)}</h3> :
+            <React.Fragment>{line}<br /></React.Fragment>
+    })}</div>
+}
+
+const Main = () => {
+    return <html>
+        <body>
+            <div className='container'>
+                <div className='row'>
+                    <h1>aaa</h1>
+                </div>
+                <form className='row align-items-end'>
+                    <div className='col-10'>                    
+                        <label htmlFor='page-url' className='form-label'>Page URL</label>
+                        <input type='text' className='form-control' id='page-url' />
+                    </div>
+                    <div className='col-auto'>
+                        <button className='btn btn-primary'>更新</button>
+                    </div>
+                </form>
+            </div>
+            <div className='container'>
+                <h2>本文</h2>
+                <div className='d-grid gap-3 container border'>
+                    <Content/>
+                </div>
+            </div>
+        </body>
+    </html>
 }
 
 export default Main
