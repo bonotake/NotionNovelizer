329f84329f5d18652430d33f9922722247ad624c
Takeo Imai <takeo.bono@gmail.com>
2021-06-21 10:08:23 +0900

    handle heading lines / add copy-to-clipboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react'
-import { Block, ParagraphBlock, RichText } from '@notionhq/client/build/src/api-types'
+import React, { useState, useRef } from 'react'
+import { Block, ParagraphBlock, HeadingOneBlock, HeadingTwoBlock, HeadingThreeBlock, RichText }
+    from '@notionhq/client/build/src/api-types'
 import useSWR from 'swr'
 
 
@@ -34,9 +35,35 @@ class Converter {
                 typeof b === 'object' &&
                 b.type == 'paragraph';
         }
+        function isH1(b: any): b is HeadingOneBlock {
+            return b !== undefined &&
+                typeof b === 'object' &&
+                b.type == 'heading_1';
+        }
+        function isH2(b: any): b is HeadingTwoBlock {
+            return b !== undefined &&
+                typeof b === 'object' &&
+                b.type == 'heading_2';
+        }
+        function isH3(b: any): b is HeadingThreeBlock {
+            return b !== undefined &&
+                typeof b === 'object' &&
+                b.type == 'heading_3';
+        }
         if (isParagraph(block)) {
             const result = block.paragraph.text.map((txt: RichText) => txt.plain_text).join('\n');
-            return this.indent(result);
+            return (result == '***') ? '[newpage]' : this.indent(result);
+        }
+        else if (isH1(block)) {
+            const result = block.heading_1.text.map((txt: RichText) => txt.plain_text).join('\n');
+            return `[chapter: ${result}]`;
+        }
+        else if (isH2(block)) {
+            const result = block.heading_2.text.map((txt: RichText) => txt.plain_text).join('\n');
+            return `[chapter: ${result}]`;
+        }
+        else if (isH3(block)) {
+            return '';  // ignore this block
         }
         else {
             console.log('unknown block type: ${block.type}');
@@ -50,17 +77,13 @@ class Converter {
 }
 
 
-const App: React.FC = ({ children }) => {
+const App: React.FC = () => {
     const [pageId, setPageId] = useState('');
     const [url, setUrl] = useState('');
+    const [text, setText] = useState('');
 
-    type InputProp = {
-        setPageId: (arg: string) => void;
-        url: string;
-        setUrl: (arg: string) => void;
-    }
 
-    const Input: React.FC<InputProp> = ({ setPageId, url, setUrl }) => {
+    const Input: React.FC<{}> = () => {
         let input: HTMLInputElement;
 
         const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -75,7 +98,13 @@ const App: React.FC = ({ children }) => {
         return <span>
             <div className='container'>
                 <div className='row'>
-                    <h1>aaa</h1>
+                    <h1>Notionで小説を</h1>
+                    <div>
+                        <p>
+                            Notionで書いた日本語の文章を、それとなく日本語文書風に閲覧・エクスポートするためのサイトです。<br/>
+                            各小説サイトやフォーマットに合うよう、よしなに変換します。
+                        </p>
+                    </div>
                 </div>
                 <form className='row align-items-end' onSubmit={handleSubmit}>
                     <div className='col-10'>                    
@@ -91,45 +120,57 @@ const App: React.FC = ({ children }) => {
         </span>
     }   
 
-    type ContentProp = {
-        pageID: string
-    }
 
-    const Content: React.FC<ContentProp> = ({ pageID, children }) => {
-        const { data, error } = useSWR(`/api/${pageId}`);
+    const Manipulate: React.FC<{}> = () => {
+        const handleCopy = async () => {
+            await navigator.clipboard.writeText(text);
+        }
 
-        if (error) return <div>failed to load</div>;
-        if (!data) return <div>loading...</div>;
-
-        const text = new Converter(data.blocks).text;
-        const lines = text.split('\n');
-        console.log(lines.length);
-        const result = lines.map((line) => <React.Fragment>{line}<br/></React.Fragment>);
-        
         return <span>
-            <div className='container'>
-                <h2>本文</h2>
-                <div className='d-grid gap-3 container border'>
-                    {result}
+            <div className='container row justify-align-end'>
+                <div className='col-8'>
+                </div>
+                <div className='col-4'>
+                    <button className='btn btn-primary' onClick={handleCopy}>
+                        クリップボードにコピー
+                    </button>
                 </div>
             </div>
         </span>
     }
 
+    const Content: React.FC<{}> = () => {
+        const format = (elem: JSX.Element | JSX.Element[]) => 
+            <span>
+                <div className='container'>
+                    <h2>本文</h2>
+                    <div className='d-grid gap-3 container border'>
+                        {elem}
+                    </div>
+                </div>
+            </span>
+
+        const { data, error } = useSWR(`/api/${pageId}`);
+
+        if (error) return format(<div>failed to load</div>);
+        if (!data) return format(<div>loading...</div>);
+
+        const text = new Converter(data.blocks).text;
+        const lines = text.split('\n');
+        console.log(lines.length);
+        const result = lines.map((line) => <React.Fragment>{line}<br/></React.Fragment>);
+        setText(text);
+
+        return format(result)
+    }
+
     return <html>
         <body>
-            <Input setPageId={setPageId} url={url} setUrl={setUrl}/>
-            <Content pageID={pageId}/>
+            <Input/>
+            <Manipulate/>
+            <Content/>
         </body>
     </html>        
-}
-
-const Main = () => {
-    return <html>
-        <body>
-            <App/>
-        </body>
-    </html>
 }
 
 export default App
