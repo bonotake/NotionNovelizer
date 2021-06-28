6d7e66c132fb552d92fef47ad301a7cf48bb887b
Takeo Imai <takeo.bono@gmail.com>
2021-06-28 23:49:03 +0900

    reintroduce bootstrap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,13 @@ import React, { FormEvent } from "react";
 import { useForm } from "react-hook-form";
 import useSWR from "swr";
 import Converter from "libs/convert";
+import { Button, Form } from "react-bootstrap";
 
 
 export default function App() {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data: FormEvent) => console.log(data);
-    const url = !watch('url') ? '' : watch('url');
+    const url = '';
+    const onSubmit = () => { url = watch('url') };
     const len = url.length;
     let pageId = "";
     if (url.includes('notion.so') && len >= 32) {
@@ -37,19 +38,28 @@ export default function App() {
     }
 
     return (
-        <React.Fragment>
-            <h1>Notionで小説を</h1>
-            <p>
-                Notionで書いた日本語の文章を、それとなく日本語文書風に閲覧・エクスポートするためのサイトです。<br/>
-                各小説サイトやフォーマットに合うよう、よしなに変換します。<br/>
-                絶賛開発中。
-            </p>
-            <form onSubmit={handleSubmit<FormEvent>(onSubmit)}>
-                <label htmlFor='page-url'>Page URL</label>
-                <input defaultValue="" {...register("url")} />
-            </form>
-            <button onClick={handleCopy}>クリップボードにコピー</button>
-            <div>{elem}</div>    
-        </React.Fragment>
+        <html>
+            <head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    rel="stylesheet" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossOrigin="anonymous"/>
+            </head>
+            <body>
+                <h1>Notionで小説を</h1>
+                <p>
+                    Notionで書いた日本語の文章を、それとなく日本語文書風に閲覧・エクスポートするためのサイトです。<br/>
+                    各小説サイトやフォーマットに合うよう、よしなに変換します。<br/>
+                    絶賛開発中。
+                </p>
+                <Form onSubmit={handleSubmit<FormEvent>(onSubmit)}>
+                    <Form.Label htmlFor='page-url'>Page URL</Form.Label>
+                    <Form.Control defaultValue="" {...register("url")} />
+                    <Button type='submit'>送信</Button>
+                </Form>
+                <Button onClick={handleCopy}>クリップボードにコピー</Button>
+                <div>{elem}</div>    
+            </body>
+        </html>
     );
 }
